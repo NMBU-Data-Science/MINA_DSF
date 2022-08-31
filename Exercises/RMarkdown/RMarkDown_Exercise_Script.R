da0b9256cbded7ce53cc6ba879db6d7fa82e9cd1
Simon D. Schowanek <simondschowanek@gmail.com>
2022-08-31 08:37:16 +0200

    Add Rmarkdown Script

---
+++ Exercises/RMarkdown/RMarkDown_Exercise_Script.R
@@ -0,0 +1,40 @@
+    ######################################################################!
+    #                                                                    #
+    #      DSF: Example Script for Rmarkdown                             #  
+    #                                                                    #
+    ######################################################################!
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # METADATA
+    #
+    # Author: Simon D. Schowanek
+    # Description: This script creates some plots and data that will be used in an Rmarkdown File later
+    # Output: a csv file with trait information
+    # time: less than 1 minute
+    # Note: 
+    #       
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+## – Clear Memory ####
+#–––––––––––––––––––––––––––––––––––––––––––––––––
+rm(list = ls())  # clean memory (= remove)
+graphics.off()  # close graphic windows
+
+
+
+## – Setting working directory ####
+#–––––––––––––––––––––––––––––––––––––––––––––––––
+getwd()
+
+
+
+## Create and save Plot
+#–––––––––––––––––––––––––––––––––––––––––––––––––
+png("/Users/au572919/Research/NMBU/MINA_DSF/Exercises/RMarkdown/Figure1.png")
+plot(mtcars$mpg, mtcars$disp, col = "red", pch = 16, main = "this is a plot")
+dev.off()
+
+
+
